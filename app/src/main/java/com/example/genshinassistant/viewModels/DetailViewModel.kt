51c3daa9021032601ea76046c4d81d85d4174504
baptistegosselet <baptiste.gosselet.outlook.fr>
2023-12-13 18:33:14 +0100

    Debug Remember

--- app/src/main/java/com/example/genshinassistant/viewModels/DetailViewModel.kt
+++ app/src/main/java/com/example/genshinassistant/viewModels/DetailViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
--- app/src/main/java/com/example/genshinassistant/views/pages/DetailPage.kt
+++ app/src/main/java/com/example/genshinassistant/views/pages/DetailPage.kt
@@ -2,35 +2,22 @@ package com.example.genshinassistant.views
 
 
 import android.util.Log
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import com.example.genshinassistant.R
-import com.example.genshinassistant.viewModels.CharacterListViewModel
+import androidx.compose.runtime.remember
 import com.example.genshinassistant.viewModels.DetailViewModel
 
 @Composable
 fun DetailPage(nameId:String) {
     Log.d("bapt", "detailpage")
 
-    val vm = DetailViewModel(nameId);
+    //val vm = DetailViewModel(nameId);
+    val vm: DetailViewModel = remember { DetailViewModel(nameId) }
 
     Column {
         Text(text="Detail");
+        Text(text=nameId);
         Text(text = vm.character.value?.name.toString())
         Text(text = vm.character.value?.vision.toString())
     }
