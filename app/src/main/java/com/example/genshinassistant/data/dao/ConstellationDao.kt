270b9aa610b1beea4763134e33d3ab43d56220da
baptiste.gosselet.etu <baptiste.gosselet.etu@univ-lille.fr>
2024-01-16 18:29:00 +0100

    modifications mineures

--- app/src/main/java/com/example/genshinassistant/data/dao/ConstellationDao.kt
+++ app/src/main/java/com/example/genshinassistant/data/dao/ConstellationDao.kt
@@ -1,7 +1,6 @@
 package com.example.genshinassistant.data.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.genshinassistant.Utils.Companion.CONSTELLATION_TABLE
--- app/src/main/java/com/example/genshinassistant/views/components/CharacterCardRowView.kt
+++ app/src/main/java/com/example/genshinassistant/views/components/CharacterCardRowView.kt
@@ -1,14 +1,10 @@
 package com.example.genshinassistant.views
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
